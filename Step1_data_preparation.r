dab6c6113c32af5694f3598b44b70a788e97d778
jakobdestat <62878532+jakobdestat@users.noreply.github.com>
2021-07-20 09:12:34 +0200

    Add files via upload
    Updated preparation files for more recent dataset versions (V8, V9)

--- Step1_data_preparation.r
+++ Step1_data_preparation.r
@@ -1,154 +1,129 @@
-####################################################################################################
-#################Program for cleaning and preparing CEDEFOP OJA data
-#####################################################################################################
-
-# Copyright 2020 DESTATIS
-#  
-# Licensed under the EUPL, Version 1.2 or – as soon as they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
-# You may not use this work except in compliance with the Licence.
-# You may obtain a copy of the Licence at:
-#  *https://joinup.ec.europa.eu/software/page/eupl5
-#  
-# Unless required by applicable law or agreed to inwriting, software distributed under the Licence is distributed on an "AS IS" basis,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the Licence for the specific language governing permissions and limitations under the Licence.
+#######################################################################
+################ Program for the initial preparation of CEDEFOP data 
+###############################################################
 
 # Setup ------------------------------------------------
 
-path <- "alldata_june20/"
+# set path according to dataset revision
+
+#revision 9
+path <- "Data/alldata_12_2020/"
+#revision 8
+#path <- "Data/alldata_9_2020/"
+#revision 4
+#path <- "Data/alldata_3_2020/"
+#revision 1
+#path <- "Data/alldata_12_2019/"
 
 options(scipen = 999)
 
 library(Hmisc)
 library(dplyr)
-#library(networkD3)
-# library(lintr)
-# library(styler)
 library(gmodels)
 library(data.table)
-#library(sp)
-#library(sf)
 library(tidyverse)
 library(rapport)
 library(openxlsx)
 library(fst)
 library(readxl)
 
-load("OJVsample_q218_q120.rdata")
+load(paste0(path, "source_quality_assessment.rdata"))
 
-dframe <- data
-rm(data)
-setDT(dframe)
+#most recent data:
+rmonth <- 12
+ryear <- 2020
 
-# mark deduplication
-dframe$dup <- ifelse(duplicated(dframe$general_id), 1, 0)
+years <- c(2018, 2019, 2020)
+#years <- c(2018, 2019)
 
-# convert dates
+for (year in years) { 
 
-dframe$grab_date <- as.Date(dframe$grab_date, origin = "1970-01-01")
-
-dframe$expire_date <- as.Date(dframe$expire_date, origin = "1970-01-01")
-
-# add month column
-
-dframe$month <- format(as.Date(dframe$grab_date), "%Y-%m")
-
-# add quarter column 
-
-dframe <- dframe %>% mutate(qtr = paste0(year(grab_date), "-", "q", quarter(grab_date)))
-
-
-# add difference between grab and expire date
-
-dframe$diff <- dframe$expire_date - dframe$grab_date
-
-###################################################
-#####recode missings to NA============================
-#############################################
-
-empty_as_na <- function(x){
-
-  x[!str_detect(x, "")] <- NA
+  load(paste0(path, "OJVsample_", year, ".rdata"))
   
-  return(x)
+  #preparation
+  setDT(dframe)
+  
+  # mark deduplication
+  dframe$dup <- ifelse(duplicated(dframe$general_id), 1, 0)
+  
+  # dates
+  
+  dframe$grab_date <- as.Date(dframe$grab_date, origin = "1970-01-01")
+  
+  dframe$expire_date <- as.Date(dframe$expire_date, origin = "1970-01-01")
+  
+  # months
+  
+  dframe$month <- format(as.Date(dframe$grab_date), "%Y-%m")
+  
+  # quarter 
+  
+  dframe <- dframe %>% mutate(qtr = paste0(year(grab_date), "-", "q", quarter(grab_date)))
+  
+  # difference grab and expire date
+  
+  dframe$diff <- dframe$expire_date - dframe$grab_date
+  
+  ###################################################
+  #####recode missings to NA============================
+  #############################################
+  
+  empty_as_na <- function(x){
+  
+    x[!str_detect(x, "")] <- NA
+    
+    return(x)
+  }
+  
+  setDT(dframe)
+  
+  dframe <- dframe %>% mutate_at(c("companyname", "city", "idcity", "idregion", "region", "province", "idprovince", "macro_region", "idmacro_region", "idcontract", "contract", "ideducational_level", "educational_level", "idsector", "sector", "idmacro_sector", "macro_sector", "idcategory_sector", "category_sector", "idsalary", "salary", "idworking_hours", "working_hours" , "idexperience", "experience"), empty_as_na)
+  
+  setDT(dframe)
+  
+  
+  ##############################################################################
+  # Geo data cleanup ------------------------------------------------
+  #######################################################################
+  
+  
+  # load NUTS table for citystates -------------------
+  
+  citystates <- read_excel("CitystatesNUTScodes.xls")
+  citystates <- citystates[1:3,]
+  
+  # replace citystates NUTS 2 --------------------
+  temp <- unlist(lapply(dframe$idmacro_region, function(x) match(x, citystates$NUTS1_Code)))
+  
+  temp1 <- as.character(citystates$NUTS2_Code[temp])
+  temp2 <- as.character(citystates$NUTS2[temp])
+  
+  dframe$idregion[!is.na(temp1)] <- temp1[!is.na(temp1)]
+  dframe$region[!is.na(temp2)] <- temp2[!is.na(temp2)]
+  
+  
+  # replace citystates NUTS 3 --------------------
+  temp <- unlist(lapply(dframe$idregion, function(x) match(x, citystates$NUTS2_Code)))
+  
+  temp1 <- as.character(citystates$NUTS3_Code[temp])
+  temp2 <- as.character(citystates$NUTS3[temp])
+  
+  dframe$idprovince[!is.na(temp1)] <- temp1[!is.na(temp1)]
+  dframe$province[!is.na(temp2)] <- temp2[!is.na(temp2)]
+  
+  #join source quality assesment
+  dframe <- left_join(dframe, sq, by= "source")
+  
+  colnames(dframe)
+  
+  ###################################################################
+  #####Save data in .fst format
+  #################################################
+  
+  write.fst(dframe, paste0(path,"OJVsample_step1_", year, ".fst"), 100)
+  
+  rm(temp)
+  rm(temp1)
+  rm(temp2)
+  rm(dframe)
 }
-
-
-setDT(dframe)
-
-dframe <- dframe %>% mutate_at(c("companyname", "city", "idcity", "province", "idprovince", "macro_region", "idmacro_region", "idcontract", "contract", "ideducational_level", "educational_level", "idsector", "sector", "idmacro_sector", "macro_sector", "idcategory_sector", "category_sector", "idsalary", "salary", "idworking_hours", "working_hours" , "idexperience", "experience"), empty_as_na)
-
-setDT(dframe)
-
-
-##############################################################################
-# Geo data cleanup ------------------------------------------------
-#######################################################################
-
-
-# load NUTS table for citystates -------------------
-
-citystates <- read_excel("CitystatesNUTScodes.xls")
-citystates <- citystates[1:3,]
-
-# replace citystates NUTS 2 --------------------
-temp <- unlist(lapply(dframe$idmacro_region, function(x) match(x, citystates$NUTS1_Code)))
-
-temp1 <- as.character(citystates$NUTS2_Code[temp])
-temp2 <- as.character(citystates$NUTS2[temp])
-
-dframe$idregion[!is.na(temp1)] <- temp1[!is.na(temp1)]
-dframe$region[!is.na(temp2)] <- temp2[!is.na(temp2)]
-
-
-# replace citystates NUTS 3 --------------------
-temp <- unlist(lapply(dframe$idregion, function(x) match(x, citystates$NUTS2_Code)))
-
-temp1 <- as.character(citystates$NUTS3_Code[temp])
-temp2 <- as.character(citystates$NUTS3[temp])
-
-dframe$idprovince[!is.na(temp1)] <- temp1[!is.na(temp1)]
-dframe$province[!is.na(temp2)] <- temp2[!is.na(temp2)]
-
-
-colnames(dframe)
-
-###################################################################
-# save the various subsets
-####################################
-
-#str(dframe)
-
-save(dframe, file=paste0(path,"OJVsample_step1.rdata"), compress = TRUE, compression_level = 6)
-
-write.fst(dframe, paste0(path,"OJVsample_step1.fst"), 100)
-
-# deduplicated subset without superfluous information
-
-dframe <- subset(dframe, dup == 0)
-
-dframe <- dframe[, c(1:49, 51:53)]
-
-savedata <- dframe[, c(1:3, 6, 7, 10:49, 51:52)]
-
-save(savedata, file=paste0(path,"OJVsample_step1_most.rdata"), compress = TRUE, compression_level = 6)
-
-# subset without occupation information
-
-savedata <- dframe[, c(1:10, 19:52)]
-
-save(savedata, file=paste0(path,"OJVsample_step1_noocc.rdata"), compress = TRUE, compression_level = 6)
-
-
-#small subset without imputed classifications (no occupation, no education, no sector)
-
-savedata <- dframe[, c(1:10, 19:30, 39:49, 51:52)]
-
-save(savedata, file=paste0(path,"OJVsample_step1_noimput.rdata"), compress = TRUE, compression_level = 6)
-
-# subset without geo information
-
-savedata <- dframe[, c(1:18, 29:52)]
-
-save(savedata, file=paste0(path,"OJVsample_step1_nogeo.rdata"), compress = TRUE, compression_level = 6)
-
--- Step1a_Deduplication_and_sizereduction.r
+++ Step1a_Deduplication_and_sizereduction.r
@@ -1,138 +1,189 @@
-####################################################################################################
-#################Program for deduplicating, filtering and ammending CEDEFOP OJA data for data quality improvements 
-#####################################################################################################
-
-# Copyright 2020 DESTATIS
-#  
-# Licensed under the EUPL, Version 1.2 or – as soon as they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
-# You may not use this work except in compliance with the Licence.
-# You may obtain a copy of the Licence at:
-#  *https://joinup.ec.europa.eu/software/page/eupl5
-#  
-# Unless required by applicable law or agreed to inwriting, software distributed under the Licence is distributed on an "AS IS" basis,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the Licence for the specific language governing permissions and limitations under the Licence.
+#######################################################################
+################ Program for the cleanup, deduplication and initial filtering of CEDEFOP data 
+###############################################################
 
 # Setup ------------------------------------------------
-path <- "alldata_june20/"
 
 options(scipen = 999)
 
 rm(list = ls())
 
+path <- "Data/alldata_12_2020/"
+dir.create( "Results/alldata_12_2020/" , showWarnings = FALSE)
+resultspath <- "Results/alldata_12_2020/"
+
+#most recent dataset revision:
+rmonth <- 12
+ryear <- 2020
+
 gc()
 
-# install.packages("dplyr")
-# install.packages("ggplot2")
-# install.packages("xlsx")
-# install.packages("networkD3")
-# install.packages("lubridate")
-# install.packages("lintr")
-# install.packages("styler")
-
 library(Hmisc)
-library(dplyr)
 library(ggplot2)
-#library(xlsx)
 library(readxl)
-#ibrary(networkD3)
-# library(lintr)
-# library(styler)
 library(gmodels)
 library(data.table)
 library(lubridate)
 library(stringr)
 library(fst)
+library(dplyr)
+library(openxlsx)
+library(foreach)
+library(parallel)
 
+# Set multicore
+no_cores <- 32
 
-#load("OJVsample_step1.rdata")
-dframe <- read.fst("OJVsample_step1.fst", as.data.table = TRUE)
+# Initiate cluster
+cl <- makeCluster(no_cores)
 
-#index <- sample(1:nrow(dframe), 10000)
-#dframe <- dframe[index, ]
+years <- c(2018, 2019, 2020)
+years <- years[years <= ryear]
 
-setDT(dframe)
+naframe <- data.frame()
+na_after_frame <- data.frame()
 
-#count and save the share of missings by variable (before deduplication)
-#this is done for data quality monitoring
-
-na_count <- sapply(dframe, function(x) sum(length(which(is.na(x)))))
-na_count <- as.data.frame(na_count)
-na_count$variable <- rownames(na_count)
-na_count$share <- round(na_count$na_count/nrow(dframe),2)
-
-write.xlsx(na_count, file = "Results/alldata_june20/na_byvar_before_dedup.xlsx", col.names = TRUE, append = FALSE )
-
-#remove observations already marked as duplicate by CEDEFOP
-#"duplicate" observations differ in their content
-#therefore we keep, from each duplicate group, the observation with the lowest number of missing variables
-
-dframe$na_count <- rowSums(is.na(dframe))
-
-dframe <- dframe %>% group_by(general_id) %>% arrange(na_count, .by_group = TRUE)
-setDT(dframe)
-
-dframe$dup <- ifelse(duplicated(dframe$general_id), 1, 0)
-
-dframe <- dframe[dup == 0]
-
-# filter staffing agencies ---------
-
-# load list of typical elements of staffing agencies names:
-keywords <- read_excel("Keywords_staffing_firms.xls" )
-
-lengtha <- sum(!is.na(keywords$combinea))
-lengthb <- sum(!is.na(keywords$combineb))
-lengthc <- sum(!is.na(keywords$identified))
-
-aword <- as.character(keywords$combinea[1:lengtha])
-bword <- as.character(keywords$combineb[1:lengthb])
-clear <- as.character(keywords$identified[1:lengthc])
-
-
-grid <- CJ(aword,bword)
-
-c_1 <- character()
-c_2 <- character()
-for (i in 1:dim(grid)[1]) {
-  c_1[i] <- paste(grid[i,1],grid[i,2])
-  c_2[i] <- paste0(grid[i,1],grid[i,2])
+for (year in years) { 
+  
+  dframe <- read.fst(paste0(path,"OJVsample_step1_", year, ".fst"), as.data.table = TRUE)
+  
+  #index <- sample(dframe$general_id, 1000)
+  #dframe <- dframe[general_id %in% index, ]
+  
+  #share of missings by variable (before deduplication)
+  
+  na_count <- unlist(mclapply(dframe, function(x) sum(length(which(is.na(x)))), mc.cores=32))
+  na_count <- as.data.frame(na_count)
+  na_count$variable <- rownames(na_count)
+  na_count$total <- nrow(dframe)
+  
+  temp <- data.frame(na_count, year)
+  
+  naframe <- rbind(naframe, temp)
+  
+  # the duplicate observations with the lowest number of missing variables
+  dframe$na_count <- rowSums(is.na(dframe))
+  
+  dframe <- dframe %>% group_by(general_id) %>% arrange(na_count, .by_group = TRUE)
+  setDT(dframe)
+  
+  dframe$dup <- ifelse(duplicated(dframe$general_id), 1, 0)
+  
+  #check manually for duplicated observations by observables
+  
+  vdup <- duplicated(subset(dframe, select = c("grab_date", "expire_date", "lang", "idesco_level_4", "esco_level_4", "idesco_level_3", "esco_level_3", "idesco_level_2", "esco_level_2", "idesco_level_1", "esco_level_1", "idprovince", "province", "idregion", "region", "idmacro_region", "macro_region", "idcountry", "country", "idcontract",     "contract", "idsector", "sector", "idmacro_sector", "macro_sector", "idcategory_sector", "category_sector", "sourcecountry","companyname" )))
+  
+  table(vdup)
+  
+  dframe$vdup <- vdup
+  
+   # mark staffing agencies ---------
+  
+  # load list of typical elements of staffing agencies names:
+  keywords <- read_excel("Keywords_staffing_firms.xls" )
+  
+  lengtha <- sum(!is.na(keywords$combinea))
+  lengthb <- sum(!is.na(keywords$combineb))
+  lengthc <- sum(!is.na(keywords$identified))
+  
+  aword <- as.character(keywords$combinea[1:lengtha])
+  bword <- as.character(keywords$combineb[1:lengthb])
+  clear <- as.character(keywords$identified[1:lengthc])
+  
+  
+  grid <- CJ(aword,bword)
+  
+  c_1 <- character()
+  c_2 <- character()
+  for (i in 1:dim(grid)[1]) {
+    c_1[i] <- paste(grid[i,1],grid[i,2])
+    c_2[i] <- paste0(grid[i,1],grid[i,2])
+  }
+  
+  keys <- c(c_1,c_2,clear)
+  
+  # removal of rows with companyname in list:
+  
+  #first step cleaning of company names
+  
+  dframe$companyname <- unlist(mclapply(dframe$companyname, function(x) gsub("[[:punct:]]|\t|\n|\r|,|;|\\.","",x), mc.cores=32))
+  
+  dframe$companyname <- tolower(dframe$companyname)
+  
+  collapsekeys <- paste(keys, collapse = '|')
+  
+  dframe$staffing <- unlist(mclapply(dframe$companyname, function(x) str_detect(x, collapsekeys), mc.cores=32))
+  
+  setDT(dframe)
+  
+  # clean and order company names for LMC --------
+  
+  dframe$companyname <- tolower(dframe$companyname)
+  
+  sep <- function(linha) {
+    resp <- strsplit(linha," |/|-")
+    resp <- unlist(resp)
+    resp <- gsub("[[:punct:]]|\t|\n|\r|,|;|\\.","",resp)
+    resp <- sort(resp[which(nchar(resp) > 2)])
+    paste0(resp,collapse=" ")
+  }
+  
+  ordered <- unlist(mclapply(dframe$companyname, function(x) sep(x), mc.cores=32))
+  
+  dframe$companyname <- ordered
+  
+  dframe$companyname[dframe$companyname==""] <- NA
+  
+  #second run of companyname filtering for staffing agencies after name cleanup
+  tmp<- unlist(mclapply(dframe$companyname, function(x) str_detect(x, collapsekeys), mc.cores=32))
+  
+  dframe[dframe$staffing == FALSE & tmp == TRUE, ]$staffing <- TRUE
+  
+  setDT(dframe)
+  
+   #share of missings by variable (AFTER deduplication and sizereduction)
+  
+  na_count <- unlist(mclapply(dframe, function(x) sum(length(which(is.na(x)))), mc.cores=32))
+  na_count <- as.data.frame(na_count)
+  na_count$variable <- rownames(na_count)
+  na_count$total <- nrow(dframe)
+  
+  #na after sizereduction
+  temp <- data.frame(na_count, year)
+  
+  na_after_frame <- rbind(naframe, temp)
+  
+  
+  # save data
+  
+  save(dframe, file=paste0(path, "OJVsample_step1_marked_", year, ".rdata" ), compress = TRUE)
+  
+  write_fst(dframe, path = paste0(path,"OJVsample_step1_marked_", year, ".fst"), compress = 100)
+  
+  # save data without interns, duplicates and staffing agencies
+  
+  dframe <- dframe[dup == 0]
+  
+  dframe <- dframe[staffing == FALSE | is.na(companyname)]
+  
+  dframe <- subset(dframe, contract!="Internship" | is.na(contract))
+  
+  save(dframe, file=paste0(path, "OJVsample_step1_redux_", year, ".rdata" ), compress = TRUE)
+  
+  write_fst(dframe, path = paste0(path,"OJVsample_step1_redux_", year, ".fst"), compress = 100)
+  
 }
 
-keys <- c(c_1,c_2,clear)
+  
+  
+#merge and save na counts of all years combined
 
-# removal of rows with companyname in list:
-
-ismiss <- dframe[is.na(companyname)]
-
-dframe <- filter(dframe, !str_detect(companyname, paste(keys, collapse = '|')))
-
-dframe <- bind_rows(dframe, ismiss)
-
-setDT(dframe)
-
-rm(ismiss)
-
-
-# clean and order company names for LMC index --------
-
-sep <- function(linha) {
-  resp <- strsplit(linha," |/|-")
-  resp <- unlist(resp)
-  resp <- gsub(",|;|\\.","",resp)
-  resp <- sort(resp[which(nchar(resp) > 2)])
-  paste0(resp,collapse=" ")
-}
-
-ordered <- sapply(dframe$companyname, function(x) sep(x))
-
-dframe$companyname <- ordered
-
-# drop internships -----------
-
-dframe <- subset(dframe, contract!="Internship" | is.na(contract))
-
-# by saving the data with the fst package, saving and loading is faster.
-# as an additional advantage, the fst package can selectively load only parts of the dataframe, for instance if only certain columns are needed for analysis
-write_fst(dframe, path = paste0(path,"OJVsample_step1_redux.fst"), compress = 100)
-
+  naframe$share <- round(naframe$na_count/naframe$total,2)
+  
+  write.xlsx(naframe, file = paste0(resultspath, "na_byvar_before_dedup.xlsx"), col.names = TRUE, append = FALSE )
+  
+  na_after_frame$share <- round(na_after_frame$na_count/na_after_frame$total,2)
+  write.xlsx(na_after_frame, file = paste0(resultspath, "na_byvar_after_dedup.xlsx"), col.names = TRUE, append = FALSE )
+  
+  
+  rm(list = ls())
